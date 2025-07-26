3870ab5fb8f36e5aa554585b064eea43606bc78f
Lucas Ribeiro da Luz <lucasrluz.23@gmail.com>
2025-07-26 12:58:29 -0300

    test: adiciona novo teste e modificação na estrutura

--- test/test.sh
+++ test/test.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
 
-mkdir test_dir
+list_contents() {
+	mkdir test_dir
 
-touch test_dir/a.txt
+	touch test_dir/a.txt
 
-cp main test_dir
+	cp main test_dir
 
-cd test_dir
+	cd test_dir
 
-output=$(./main)
+	output=$(./main)
 
-if echo "$output" | grep -q "main" && echo "$output" | grep -q "a.txt"; then
-	echo "Teste passou"
-else
-	echo "Teste falhou"
-	echo "Output:"
-	echo "$output"
-fi
+	if echo "$output" | grep -q "main" && echo "$output" | grep -q "a.txt"; then
+		echo "Teste passou"
+	else
+		echo "Teste falhou"
+		echo "Output:"
+		echo "$output"
+	fi
 
-cd ..
+	cd ..
 
-rm -rf test_dir
+	rm -rf test_dir
+}
 
+list_contents_with_path_is_provided() {
+	mkdir test_dir
+
+	touch test_dir/a.txt
+
+	output=$(./main test_dir)
+
+	if echo "$output" | grep -q "a.txt"; then
+		echo "Teste passou"
+	else
+		echo "Teste falhou"
+		echo "Output:"
+		echo "$output"
+	fi
+
+	rm -rf test_dir
+}
+
+list_contents
+list_contents_with_path_is_provided
